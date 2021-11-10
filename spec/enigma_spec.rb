233a4de715eef5cb285edcc25e23436fc1e8dbac
Mallory Vining <mallorygallery12@gmail.com>
2021-11-10 11:47:14 -0700

    test:Install SimpleCov

---
+++ spec/enigma_spec.rb
@@ -0,0 +1,11 @@
+require "simplecov"
+require "spec_helper"
+require "rspec"
+require "./lib/enigma"
+
+describe Enigma do
+  it 'exists' do
+    enigma = Enigma.new
+    expect(enigma).to be_an_instance_of(Enigma)
+  end
+end
